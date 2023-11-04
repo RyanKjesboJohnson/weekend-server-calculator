c6350d1d14bf8767ab442ba0ce62b6a9573899e9
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-11-03 20:10:49 -0500

    completed initial setup, started TODO list file, and create first two client side functions

---
+++ TODO.md
@@ -0,0 +1,17 @@
+1. Setup all necessary files/folders for the application:
+   X Public: css, html, js
+   X server.js
+   X .gitignore
+2. Setup server:
+    X Run express
+    X Setup boiler plate language
+    Create an array for the calculation history
+    Create a axios get handle to pull out calculation history
+    Create an axios get handle to pull out the calculation response
+    Write a function that will take in two number elements and a calculation element, and respond with the response
+    Create an axios pull handle to receive the calculation object
+3. Setup the html
+    Create two input fields, create four function buttons, and an equal/clear button
+    Create a header for the total, and a div under for the result to live.
+    Create a header for the history, and a div under for the history to live.
+4. Setup the client.js side
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,31 @@
+
 console.log('client.js is sourced!');
+
+//Client side functions go here
+
+//These are the client side functions (in order alphabetically
+
+//This function removes the active class from any operator.
+//This function appears in functions: selectThisOperator, onReady
+function inactivateOperators() {
+    console.log("this is the inactivateOperators function");
+    document.getElementById('plus').classList.remove("active");
+    document.getElementById('minus').classList.remove("active");
+    document.getElementById('times').classList.remove("active");
+    document.getElementById('divide').classList.remove("active");
+}
+
+//This function will run when someone clicks on an operator, it will
+// run the unclick function first to remove active from any operator,
+// then will apply the active css class to the button pressed.
+function selectThisOperator(event){
+    event.preventDefault();
+    console.log("this is the selectthisoperator function");
+    // console.log(event.target.classList);
+    inactivateOperators(event);
+    const list = event.target.classList;
+    // console.log(list);
+    event.target.classList.value = "active";
+}
+
+
